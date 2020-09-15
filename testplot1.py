366812462083cc00317f2518063b8d778bff765d
Gani Annaberdiyev <aannabe@ncsu.edu>
2020-09-15 10:51:07 -0400

    read_data_from_csv

--- testplot1.py
+++ testplot1.py
@@ -14,17 +14,16 @@ import re
 import string
 
 styles = {
-'2enoTmoves' :{'label':'2e- ccECP no Tmoves', 'color':'#ff9933', 'linestyle':'--', 'dashes': (3,1), 'marker':'s', 'markersize':6, 'markeredgecolor':'#000000', 'markeredgewidth':0.5},
-'2eTmoves':{'label':'2e- ccECP Tmoves','color':'#009933', 'linestyle':'--', 'dashes': (6,1), 'marker':'o', 'markersize':6, 'markeredgecolor':'#000000', 'markeredgewidth':0.5},
+'2enoTmoves' :{'label':'2e$^-$ ccECP no T-moves', 'color':'#ff9933', 'linestyle':'--', 'dashes': (3,1), 'marker':'s', 'markersize':6, 'markeredgecolor':'#000000', 'markeredgewidth':0.5},
+'2eTmoves'   :{'label':'2e$^-$ ccECP T-moves',    'color':'#009933', 'linestyle':'--', 'dashes': (6,1), 'marker':'o', 'markersize':6, 'markeredgecolor':'#000000', 'markeredgewidth':0.5},
 }
 
-
 def init():
-    font   = {'family' : 'serif', 'size': 20}
-    lines  = {'linewidth': 2.5}
+    font   = {'family' : 'serif', 'size': 18}
+    lines  = {'linewidth': 2.0}
     axes   = {'linewidth': 2.0}    # border width
     tick   = {'major.size': 2, 'major.width':2}
-    legend = {'frameon':False, 'fontsize':16.0, 'handlelength':2.20, 'labelspacing':0.40, 'handletextpad':0.4, 'loc':'best'}
+    legend = {'frameon':False, 'fontsize':17.0, 'handlelength':2.30, 'labelspacing':0.40, 'handletextpad':0.4, 'loc':'best'}
 
     mpl.rc('font',**font)
     mpl.rc('lines',**lines)
@@ -43,26 +42,72 @@ def init():
     #ax2 = plt.subplot(gs[1], sharex = ax1)
     return fig, ax1
 
-##########################DATA#######################
+###  ~~~~~~~ Functions ~~~~~~~~~
 
-timestep = [0.001, 0.002, 0.003, 0.005, 0.008, 0.01, 0.02, 0.045, 0.05, 0.055, 0.07, 0.09, 0.1, 0.2, 0.3, 0.5]
-energy = [-2.2454, -2.2448, -2.2448, -2.2438, -2.2451, -2.2456, -2.2464, -2.2464, -2.2464, -2.2469, -2.2476, -2.2481, -2.2472, -2.2525,-2.2519, -2.2568]
-err =[0.0006,0.0009,0.0005,0.0003,0.0005,0.0005,0.0009,0.0004,0.0006,0.0004,0.0008,0.0004,0.0007,0.001,0.0006,0.0008]
+class ShorthandFormatter(string.Formatter):
+    def format_field(self, value, format_spec):
+        if isinstance(value, uncertainties.UFloat):
+            return value.format(format_spec+'S')  # Shorthand option added
+        # Special formatting for other types can be added here (floats, etc.)
+        else:
+            # Usual formatting:
+            return super(ShorthandFormatter, self).format_field(
+                value, format_spec)
+frmtr = ShorthandFormatter()
 
-#########################Plot#########################
+def f2s(x):   # float to string
+    try:
+        y = frmtr.format("{0:.1u}", x)
+    except:
+        y = x
+    return y
+
+def s2f(x):   # string to float
+    try:
+        y = ufloat_fromstr(x)
+    except:
+        y = x
+    return y
+
+def pd_s2f(df):   # df to df with uncertainties
+    udf = df.copy()
+    for column in df.columns:
+        try:
+            udf[column] = list(map(s2f,list(df[column])))
+        except:
+            udf[column] = "pd_s2f failed"
+    return udf
+
+def pd_f2s(udf):   # df to df with uncertainties
+    df = udf.copy()
+    for column in udf.columns:
+        try:
+            df[column] = list(map(f2s,list(udf[column])))
+        except:
+            df[column] = "pd_f2s failed"
+    return df
+
+### ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+ccECP_2e_TM = pd.read_csv('2eccECPTmoves.csv')
+ccECP_2e_TM = pd_s2f(ccECP_2e_TM)
+print(ccECP_2e_TM)
 
 fig, ax1 = init()
-ax1.tick_params(direction='in', length=8, width=2, which='major', pad=10)
-ax1.tick_params(direction='in', length=4, width=1, which='minor', pad=10)
-ax1.set_ylabel('Energy and Errorbar (Hatree)')
-ax1.set_xlabel('$Timestep (Hatree$^{-1}$)$')
-tick_spacing = 1.0
-ax1.yaxis.set_major_locator(ticker.MultipleLocator(tick_spacing))
-ax1.set_ylim(-2.2600, -2.2400)
+ax1.tick_params(direction='in', length=8, width=2.0, which='major', pad=10)
+ax1.tick_params(direction='in', length=4, width=1.1, which='minor', pad=10)
+ax1.set_ylabel('Energy [Ha]')
+ax1.set_xlabel('Timestep [Ha$^{-1}$]')
+ax1.set_xscale("log")
 
-ax1.errorbar(timestep, energy, yerr=err, **styles['2enoTmoves'])
+timestep = ccECP_2e_TM['timestep']
+energy = unumpy.nominal_values(np.array(ccECP_2e_TM['energy'].values))
+errors = unumpy.std_devs(np.array(ccECP_2e_TM['energy'].values))
 
-ax1.axhline(y=0.0, ls='--', lw=1.0, color='#ff0000', alpha=0.5)
+ax1.errorbar(timestep, energy, yerr=errors, **styles['2eTmoves'])
+
 ax1.legend(ncol=1)
-ax1.grid(b=None, which='major', axis='both', alpha=0.2)          
-plt.savefig('2eccECPnoTmoves.pdf')
+ax1.grid(b=None, which='major', axis='both', alpha=0.1)
+plt.savefig('2eccECPTmoves.pdf')
+plt.show()
+
